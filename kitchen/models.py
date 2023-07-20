5274d39062839a6266ab900f4c26beb02e238a15
YOUR_NAME <YOUR_EMAIL>
2023-07-20 21:19:11 +0300

    Add models: DishType(models.Model) Dish(models.Model) Cook(AbstractUser)

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+from kitchen_service import settings
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} ({self.price})"
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("kitchen:cook-detail", kwargs={"pk": self.pk})

7ee9934e2af4e155ee7c968b470cb90919d52b0d
YOUR_NAME <YOUR_EMAIL>
2023-07-20 21:16:09 +0300

    add base project files

--- kitchen_service/urls.py
+++ kitchen_service/urls.py
@@ -1,4 +1,4 @@
-"""kitchen_servise URL Configuration
+"""kitchen_service URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/

697eb9450e749e4d9e33d35088c4ffe0e5f96a4c
YOUR_NAME <YOUR_EMAIL>
2023-07-21 11:20:39 +0300

    added urls CRUD operations for cook, and index page

---
+++ kitchen/urls.py
@@ -0,0 +1,41 @@
+from django.urls import path
+
+from .views import (
+    index,
+    CookListView,
+    CookDetailView,
+    CookCreateView,
+    CookDeleteView,
+    CookUpdateView,
+)
+
+urlpatterns = [
+    path("", index, name="index"),
+    path(
+        "cooks/",
+        CookListView.as_view(),
+        name="cook-list"
+    ),
+    path(
+        "cooks/<int:pk>/",
+        CookDetailView.as_view(),
+        name="cook-detail"
+    ),
+    path(
+        "cooks/create/",
+        CookCreateView.as_view(),
+        name="cook-create"
+    ),
+    path(
+        "cooks/<int:pk>/update/",
+        CookUpdateView.as_view(),
+        name="cook-update",
+    ),
+    path(
+        "cooks/<int:pk>/delete/",
+        CookDeleteView.as_view(),
+        name="cook-delete",
+    ),
+]
+
+app_name = "kitchen"

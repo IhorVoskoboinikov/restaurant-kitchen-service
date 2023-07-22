fd77fb8464f506e17b373325d6d13aedaa1828e6
YOUR_NAME <YOUR_EMAIL>
2023-07-22 14:06:39 +0300

    added all urls (CRUD operations) for dishes and dish_type

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -7,6 +7,17 @@ from .views import (
     CookCreateView,
     CookDeleteView,
     CookUpdateView,
+
+    DishTypeListView,
+    DishTypeCreateView,
+    DishTypeUpdateView,
+    DishTypeDeleteView,
+
+    DishListView,
+    DishCreateView,
+    DishDetailView,
+    DishUpdateView,
+    DishDeleteView,
 )
 
 urlpatterns = [
@@ -35,6 +46,51 @@ urlpatterns = [
         "cooks/<int:pk>/delete/",
         CookDeleteView.as_view(),
         name="cook-delete",
+    ),
+    path(
+        "dish_types/",
+        DishTypeListView.as_view(),
+        name="dish-type-list"
+    ),
+    path(
+        "dish_types/create/",
+        DishTypeCreateView.as_view(),
+        name="dish-type-create"
+    ),
+    path(
+        "dish_types/<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dish-type-update",
+    ),
+    path(
+        "dish_types/<int:pk>/delete/",
+        DishTypeDeleteView.as_view(),
+        name="dish-type-delete",
+    ),
+    path(
+        "dishes/",
+        DishListView.as_view(),
+        name="dish-list"
+    ),
+    path(
+        "dishes/create/",
+        DishCreateView.as_view(),
+        name="dish-create"
+    ),
+    path(
+        "dishes/<int:pk>/",
+        DishDetailView.as_view(),
+        name="dish-detail"
+    ),
+    path(
+        "dishes/<int:pk>/update/",
+        DishUpdateView.as_view(),
+        name="dish-update",
+    ),
+    path(
+        "dishes/<int:pk>/delete/",
+        DishDeleteView.as_view(),
+        name="dish-delete",
     ),
 ]
 

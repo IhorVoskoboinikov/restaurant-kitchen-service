bca4a6b86760f4f96ac58d0956488c3209a12bb2
YOUR_NAME <YOUR_EMAIL>
2023-07-25 15:58:03 +0300

    Added restaurant-kitchen-service-mate.onrender.com to ALLOWED_HOSTS

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 # print(DEBUG)
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "restaurant-kitchen-service-mate.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
